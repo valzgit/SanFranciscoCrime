6d000758763add59643254a67a1dd73e3cfdf44e
valzgit <nikola.valzorio.1998@gmail.com>
2022-01-07 16:53:10 +0100

    enable csv export

--- main.py
+++ main.py
@@ -19,15 +19,12 @@ DataSeparator.extractAddressInfo(testData)
 
 trainCategory = DataEncoder.labelEncodeCategories(DataEncoder.fetchCategoriesAndClassifyThem(trainData))
 trainData, testData = DataEncoder.labelEncodeDistrict(trainData, testData)
-# trainCategory.drop(trainCategory.tail(875726 - 873412).index, inplace=True)
-
-# testData = DataEncoder.labelEncodeDistrict(testData)
 
 print('Dropping Id columns from Test and Result tables...')
 testData.drop(columns=['Id'], inplace=True)
 print('Success!')
 
-# CSVConverter.TrainAndTestToCSVFiles(trainData, testData)
+CSVConverter.TrainAndTestToCSVFiles(trainData, testData)
 
 solution = ModelTraining.decideTrainingPath(trainData, trainCategory, testData)
 CSVConverter.toCSVFile(solution)
